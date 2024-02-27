25f0262fcd9e0b99559f75cc3df7663d92270902
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-02-28 01:22:49 +0600

    last commit api explore

--- explore-api/js/comments.js
+++ explore-api/js/comments.js
@@ -18,3 +18,5 @@ const loadComments2 = async () => {
 // function displayComments(comments) {
 //   console.log(comments);
 // }
+const stuInfo = JSON.stringify({ name: "James", roll: 3 });
+console.log(stuInfo.name);
